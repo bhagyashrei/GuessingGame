ff51b2d247dbacea90b0c475731eb8effedfb133
Bhagyashree <bhagyashreesharma1@gmail.com>
2019-02-15 00:54:59 +0000

    final

---
+++ base.js
@@ -0,0 +1,152 @@
+var divTag1 = document.getElementById("q1");
+var div1A=document.getElementById("qf1");
+var div1B=document.getElementById("qb1");
+
+var divTag2 = document.getElementById("q2");
+var div2A=document.getElementById("qf2");
+var div2B=document.getElementById("qb2");
+
+var divTag3 = document.getElementById("q3");
+var div3A=document.getElementById("qf3");
+var div3B=document.getElementById("qb3");
+
+
+var divTag4 = document.getElementById("q4");
+var div4A=document.getElementById("qf4");
+var div4B=document.getElementById("qb4");
+
+var divTag5 = document.getElementById("q5");
+var div5A=document.getElementById("qf5");
+var div5B=document.getElementById("qb5");
+
+var divTag6 = document.getElementById("q6");
+var div6A=document.getElementById("qf6");
+var div6B=document.getElementById("qb6");
+
+var divTag7 = document.getElementById("q7");
+var div7A=document.getElementById("qf7");
+var div7B=document.getElementById("qb7");
+
+var divTag8 = document.getElementById("q8");
+var div8A=document.getElementById("qf8");
+var div8B=document.getElementById("qb8");
+
+var divTag9 = document.getElementById("q9");
+var div9A=document.getElementById("qf9");
+var div9B=document.getElementById("qb9");
+
+
+
+
+  divTag1.addEventListener("click", function(){
+ 
+
+    div1A.classList.toggle("card__side--front");
+    div1A.classList.toggle("card__side--fronter");
+  
+    div1B.classList.toggle("card__side--back");
+    div1B.classList.toggle("card__side--backer");
+   
+     
+    });
+
+    divTag2.addEventListener("click", function(){
+ 
+
+      div2A.classList.toggle("card__side--front");
+      div2A.classList.toggle("card__side--fronter");
+    
+      div2B.classList.toggle("card__side--back");
+      div2B.classList.toggle("card__side--backer");
+     
+       
+      });
+
+      divTag3.addEventListener("click", function(){
+ 
+
+        div3A.classList.toggle("card__side--front");
+        div3A.classList.toggle("card__side--fronter");
+      
+        div3B.classList.toggle("card__side--back");
+        div3B.classList.toggle("card__side--backer");
+       
+         
+        });
+
+
+        divTag4.addEventListener("click", function(){
+ 
+
+          div4A.classList.toggle("card__side--front");
+          div4A.classList.toggle("card__side--fronter");
+        
+          div4B.classList.toggle("card__side--back");
+          div4B.classList.toggle("card__side--backer");
+         
+           
+          });
+
+
+          divTag5.addEventListener("click", function(){
+ 
+
+            div5A.classList.toggle("card__side--front");
+            div5A.classList.toggle("card__side--fronter");
+          
+            div5B.classList.toggle("card__side--back");
+            div5B.classList.toggle("card__side--backer");
+           
+             
+            });
+
+
+            divTag6.addEventListener("click", function(){
+ 
+
+              div6A.classList.toggle("card__side--front");
+              div6A.classList.toggle("card__side--fronter");
+            
+              div6B.classList.toggle("card__side--back");
+              div6B.classList.toggle("card__side--backer");
+             
+               
+              });
+
+              divTag7.addEventListener("click", function(){
+ 
+
+                div7A.classList.toggle("card__side--front");
+                div7A.classList.toggle("card__side--fronter");
+              
+                div7B.classList.toggle("card__side--back");
+                div7B.classList.toggle("card__side--backer");
+               
+                 
+                });
+
+                divTag8.addEventListener("click", function(){
+ 
+                  div8A.classList.toggle("card__side--front");
+                  div8A.classList.toggle("card__side--fronter");
+                
+                  div8B.classList.toggle("card__side--back");
+                  div8B.classList.toggle("card__side--backer");
+                 
+                   
+                  });
+
+
+                  divTag9.addEventListener("click", function(){
+ 
+
+                    div9A.classList.toggle("card__side--front");
+                    div9A.classList.toggle("card__side--fronter");
+                  
+                    div9B.classList.toggle("card__side--back");
+                    div9B.classList.toggle("card__side--backer");
+                   
+                     
+                    });
+  
+
